469e0447950d8707ef11cabb38338d2daef29ad2
Junior Boone <juniorboone25@gmail.com>
2022-01-05 21:33:07 -0400

    Delete Integrador/Integrador.ConsoleApp directory

--- Integrador/Integrador.ConsoleApp/Juros/IJuros.cs
+++
@@ -1,9 +0,0 @@
-﻿using CSharpFunctionalExtensions;
-
-namespace Integrador.ConsoleApp.Juros
-{
-    public interface IJuros
-    {
-        Result<JurosCalculado> Calcular(decimal valorOriginal, int quantidadeDiasEmAtraso);
-    }
-}
